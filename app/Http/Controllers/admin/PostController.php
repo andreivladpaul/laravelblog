309d0d97ca1655c609bb9fc45dd9340414925be1
Vlad <andreivladpaul@gmail.com>
2021-11-07 17:06:22 +0100

    Edit functioning for posts

--- app/Http/Controllers/admin/PostController.php
+++ app/Http/Controllers/admin/PostController.php
@@ -74,7 +74,8 @@ class PostController extends Controller
      */
     public function edit($id)
     {
-        return view('admin.post.post');
+        $post = post::where('id',$id)->first();
+        return view('admin.post.edit', compact('post'));
     }
 
     /**
@@ -86,7 +87,21 @@ class PostController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request,[
+            'title'=>'required',
+            'subtitle'=>'required',
+            'slug'=>'required',
+            'body'=>'required',
+        ]);
+
+        $post = post::find($id);
+        $post->title = $request->title;
+        $post->subtitle = $request->subtitle;
+        $post->slug = $request->slug;
+        $post->body = $request->body;
+        $post->save();
+
+        return redirect(route('post.index'));
     }
 
     /**
---
+++ resources/views/admin/post/edit.blade.php
@@ -0,0 +1,83 @@
+@extends('admin/layouts/app')
+
+
+ @section('main-section')
+<div class="wrapper">
+    @include('admin/layouts/header')
+    @include('admin/layouts/sidebar')
+    <!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+<!-- Content Header (Page header) -->
+<section class="content">
+    <div class="row">
+      <div class="col-md-12">
+
+        <div class="card card-outline card-info">
+
+            @include('layouts.messages')
+            <form role="form"  method="POST" action="{{ route('post.update', $post->id) }}">
+                @csrf
+                @method('PUT')
+                <div class="card-body">
+                    <div class="form-group">
+                        {{-- title --}}
+                        <label for="title">Post title</label>
+                        <input type="text" name="title" value="{{ $post->title }}" class="form-control" id="title" placeholder="Enter title">
+                    </div>
+                  {{-- subtitle --}}
+                  <div class="form-group">
+                    <label for="subtitle">Post subtitle</label>
+                    <input type="text" name="subtitle" value="{{ $post->subtitle }}" class="form-control" id="subtitle" placeholder="Enter subtitle">
+                  </div>
+                  {{-- slug --}}
+                  <div class="form-group">
+                    <label for="slug">Post slug</label>
+                    <input type="text" name="slug" value="{{ $post->slug }}" class="form-control" id="slug" placeholder="Enter slug">
+                  </div>
+                  {{-- img --}}
+                  <div class="form-group">
+                    <label for="exampleInputFile">File input</label>
+                    <div class="input-group">
+                      <div class="custom-file">
+                        <input type="file" name="image" class="custom-file-input" id="image">
+                        <label class="custom-file-label" for="image">Choose image</label>
+                      </div>
+                      <div class="input-group-append">
+                        <span class="input-group-text" id="">Upload</span>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="form-check">
+                    <input type="checkbox" name="status" @if ($post->status == 1) checked  @endif class="form-check-input" id="status">
+                    <label class="form-check-label" for="status">Publish</label>
+                  </div>
+                  {{-- text area --}}
+                  <div class="card-body pad">
+                    <div class="mb-3">
+                      <textarea class="textarea"  name="body" placeholder="Place some text here"
+                                style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;">{{ $post->body }}</textarea>
+                    </div>
+
+                  </div>
+                  <br>
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                  <a class=" btn mx-3 btn-info" href="{{ route('post.index') }}">Back</a>
+                </div>
+                <!-- /.card-body -->
+
+
+              </form>
+
+          <!-- /.card-header -->
+
+        </div>
+      </div>
+      <!-- /.col-->
+    </div>
+    <!-- ./row -->
+  </section>
+
+
+</div>
+@endsection
